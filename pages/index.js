193bb7202844f3f35781f4d655f107a19db104d4
Manuel Muñoz Solera <mamuso@mamuso.net>
2021-03-27 21:59:09 -0700

    Adding a title to the page

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,12 @@
+import Head from "next/head";
 import Header from "../components/Header";
 import Instructions from "../components/Instructions";
 function HomePage() {
   return (
     <>
+      <Head>
+        <title>Simple light/dark mode in Next.js</title>
+      </Head>
       <Header />
       <Instructions />
     </>
